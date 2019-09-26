3293059b566807454e101282df89cb9dd2ced28f
gannza <respinho2014@gmail.com>
2019-09-27 00:23:20 +0200

    add basic files

--- config/app.php
+++ config/app.php
@@ -178,7 +178,8 @@ return [
         Laracasts\Flash\FlashServiceProvider::class,
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
         \InfyOm\Generator\InfyOmGeneratorServiceProvider::class,
-        \InfyOm\AdminLTETemplates\AdminLTETemplatesServiceProvider::class
+        \InfyOm\AdminLTETemplates\AdminLTETemplatesServiceProvider::class,
+        \InfyOm\GeneratorBuilder\GeneratorBuilderServiceProvider::class
 
     ],
 
