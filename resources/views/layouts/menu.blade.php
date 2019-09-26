1b6e7df74c25ea4febddb925cca254941385269b
gannza <respinho2014@gmail.com>
2019-09-27 01:48:48 +0200

    #2

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,23 +2,4 @@
     <a href="{!! route('sessions.index') !!}"><i class="fa fa-edit"></i><span>Sessions</span></a>
 </li>
 
-<li class="{{ Request::is('sessions*') ? 'active' : '' }}">
-    <a href="{!! route('sessions.index') !!}"><i class="fa fa-edit"></i><span>Sessions</span></a>
-</li>
-
-<li class="{{ Request::is('sessions*') ? 'active' : '' }}">
-    <a href="{!! route('sessions.index') !!}"><i class="fa fa-edit"></i><span>Sessions</span></a>
-</li>
-
-<li class="{{ Request::is('sessions*') ? 'active' : '' }}">
-    <a href="{!! route('sessions.index') !!}"><i class="fa fa-edit"></i><span>Sessions</span></a>
-</li>
-
-<li class="{{ Request::is('sessions*') ? 'active' : '' }}">
-    <a href="{!! route('sessions.index') !!}"><i class="fa fa-edit"></i><span>Sessions</span></a>
-</li>
-
-<li class="{{ Request::is('sessions*') ? 'active' : '' }}">
-    <a href="{!! route('sessions.index') !!}"><i class="fa fa-edit"></i><span>Sessions</span></a>
-</li>
 
