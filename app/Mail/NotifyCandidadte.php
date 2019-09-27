72012055e583ef5ecebf6731cd3b12728fca5234
Richie457 <beastar457@gmail.com>
2019-09-27 10:49:58 +0200

    send email after application

---
+++ app/Mail/NotifyCandidadte.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NotifyCandidadte extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    protected $name;
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from(env('MAIL_FROM', ''))
+            ->subject("Miss Career Application notification")
+            ->view('emails.miss.mail')
+            ->with([
+                'name' =>  $this->name
+            ]);
+    }
+}
