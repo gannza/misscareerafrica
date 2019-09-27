a8749e221c48c8bf3c574f85d8e1ec8f90ebac99
gannza <respinho2014@gmail.com>
2019-09-27 06:16:11 +0200

    ..

--- resources/views/candidates/show_fields.blade.php
+++ resources/views/candidates/show_fields.blade.php
@@ -1,178 +1,187 @@
 <!-- Id Field -->
 <div class="row">
-<div class="col-md-6">
-<div class="col-md-12">
-										<b>Personal Info</b>
-										<hr />
-									</div>
-<div class="form-group">
-  
-    <p>  {!! Form::label('id', 'Id:') !!}: {!! $candidate->id !!}</p>
-</div>
+    <div class="col-md-6">
+        <div class="col-md-12">
+            <hr />
+            <b>Personal Info</b>
+            <hr />
+        </div>
+        <div class="form-group">
 
-<!-- Fname Field -->
-<div class="form-group">
-   
-    <p> {!! Form::label('fname', 'First name:') !!}: {!! $candidate->fname !!}</p>
-</div>
+            <p> {!! Form::label('id', 'Id:') !!}: {!! $candidate->id !!}</p>
+        </div>
 
-<!-- Lname Field -->
-<div class="form-group">
-   
-    <p> {!! Form::label('lname', 'Last name:') !!}: {!! $candidate->lname !!}</p>
-</div>
+        <!-- Fname Field -->
+        <div class="form-group">
 
-<!-- Email Field -->
-<div class="form-group">
-   
-    <p> {!! Form::label('email', 'Email:') !!} : {!! $candidate->email !!}</p>
-</div>
+            <p> {!! Form::label('fname', 'First name:') !!}: {!! $candidate->fname !!}</p>
+        </div>
 
-<!-- Dob Field -->
-<div class="form-group">
-   
-    <p> {!! Form::label('dob', 'Dob:') !!}: {!! $candidate->dob !!}</p>
-</div>
+        <!-- Lname Field -->
+        <div class="form-group">
 
-<!-- Phone Code Field -->
-<div class="form-group">
-   
-    <p> {!! Form::label('phone_code', 'Phone Code:') !!}: {!! $candidate->phone_code !!}</p>
-</div>
+            <p> {!! Form::label('lname', 'Last name:') !!}: {!! $candidate->lname !!}</p>
+        </div>
 
-<!-- Phone Number Field -->
-<div class="form-group">
-   
-    <p> {!! Form::label('phone_number', 'Phone Number:') !!}: {!! $candidate->phone_number !!}</p>
-</div>
+        <!-- Email Field -->
+        <div class="form-group">
 
-<!-- Street Field -->
-<div class="form-group">
-   
-    <p> {!! Form::label('street', 'Street:') !!}: {!! $candidate->street !!}</p>
-</div>
+            <p> {!! Form::label('email', 'Email:') !!} : {!! $candidate->email !!}</p>
+        </div>
 
-<!-- City Field -->
-<div class="form-group">
-   
-    <p> {!! Form::label('city', 'City:') !!}: {!! $candidate->city !!}</p>
-</div>
+        <!-- Dob Field -->
+        <div class="form-group">
 
-<!-- Province Field -->
-<div class="form-group">
-    
-    <p>{!! Form::label('province', 'Province:') !!}: {!! $candidate->province !!}</p>
-</div>
+            <p> {!! Form::label('dob', 'Dob:') !!}: {!! $candidate->dob !!}</p>
+        </div>
 
-<!-- Country Field -->
-<div class="form-group">
-   
-    <p> {!! Form::label('country', 'Country:') !!}: {!! $candidate->country !!}</p>
-</div>
+        <!-- Phone Code Field -->
+        <div class="form-group">
 
-<!-- Level Education Field -->
-<div class="form-group">
-  
-    <p>  {!! Form::label('level_education', 'Level of education:') !!}: {!! $candidate->level_education !!}</p>
-</div>
+            <p> {!! Form::label('phone_code', 'Phone Code:') !!}: {!! $candidate->phone_code !!}</p>
+        </div>
 
-<!-- Former School Attended Field -->
-<div class="form-group">
-   
-    <p> {!! Form::label('former_school_attended', 'Former School Attended:') !!}: {!! $candidate->former_school_attended !!}</p>
-</div>
+        <!-- Phone Number Field -->
+        <div class="form-group">
 
-<!-- Education Background Field -->
-<div class="form-group">
-    {!! Form::label('education_background', 'Briefly describe your education background (500 Max)') !!}
-    <p>{!! $candidate->education_background !!}</p>
-</div>
+            <p> {!! Form::label('phone_number', 'Phone Number:') !!}: {!! $candidate->phone_number !!}</p>
+        </div>
 
-<!-- Current Occupation Field -->
-<div class="form-group">
-    {!! Form::label('current_occupation', 'Current Occupation') !!}
-    <p>{!! $candidate->current_occupation !!}</p>
-</div>
-</div>
-<div class="col-md-6">
+        <!-- Street Field -->
+        <div class="form-group">
+
+            <p> {!! Form::label('street', 'Street:') !!}: {!! $candidate->street !!}</p>
+        </div>
+
+        <!-- City Field -->
+        <div class="form-group">
+
+            <p> {!! Form::label('city', 'City:') !!}: {!! $candidate->city !!}</p>
+        </div>
+
+        <!-- Province Field -->
+        <div class="form-group">
+
+            <p>{!! Form::label('province', 'Province:') !!}: {!! $candidate->province !!}</p>
+        </div>
+
+        <!-- Country Field -->
+        <div class="form-group">
+
+            <p> {!! Form::label('country', 'Country:') !!}: {!! $candidate->country !!}</p>
+        </div>
+
+        <!-- Level Education Field -->
+        <div class="form-group">
+
+            <p> {!! Form::label('level_education', 'Level of education:') !!}: {!! $candidate->level_education !!}</p>
+        </div>
+
+        <!-- Former School Attended Field -->
+        <div class="form-group">
+
+            <p> {!! Form::label('former_school_attended', 'Former School Attended:') !!}: {!!
+                $candidate->former_school_attended !!}</p>
+        </div>
+
+        <!-- Education Background Field -->
+        <div class="form-group">
+            {!! Form::label('education_background', 'Briefly describe your education background (500 Max)') !!}
+            <p>{!! $candidate->education_background !!}</p>
+        </div>
+
+        <!-- Current Occupation Field -->
+        <div class="form-group">
+            {!! Form::label('current_occupation', 'Current Occupation') !!}
+            <p>{!! $candidate->current_occupation !!}</p>
+        </div>
+    </div>
+    <div class="col-md-6">
 
 
-<div class="col-md-12">
-											<hr />
-											<b>MISS CAREER AFRICA COMPETITION SECTION</b>
-										<hr />
-									</div>
-<!-- Q1 Field -->
-<div class="form-group">
-    {!! Form::label('q1', 'Q1. Tell us about yourself? (500 Max)') !!}
-    <p>{!! $candidate->q1 !!}</p>
-</div>
+        <div class="col-md-12">
+            <hr />
+            <b>MISS CAREER AFRICA COMPETITION SECTION</b>
+            <hr />
+        </div>
+        <!-- Q1 Field -->
+        <div class="form-group">
+            {!! Form::label('q1', 'Q1. Tell us about yourself? (500 Max)') !!}
+            <p>{!! $candidate->q1 !!}</p>
+        </div>
 
-<!-- Q2 Field -->
-<div class="form-group">
-    {!! Form::label('q2', 'Q2. What is your next five years career plans? (500 Max)') !!}
-    <p>{!! $candidate->q2 !!}</p>
-</div>
+        <!-- Q2 Field -->
+        <div class="form-group">
+            {!! Form::label('q2', 'Q2. What is your next five years career plans? (500 Max)') !!}
+            <p>{!! $candidate->q2 !!}</p>
+        </div>
 
-<!-- Q3 Field -->
-<div class="form-group">
-    {!! Form::label('q3', 'Q3. What is your project name?') !!}
-    <p>{!! $candidate->q3 !!}</p>
-</div>
+        <!-- Q3 Field -->
+        <div class="form-group">
+            {!! Form::label('q3', 'Q3. What is your project name?') !!}
+            <p>{!! $candidate->q3 !!}</p>
+        </div>
 
-<!-- Q4 Field -->
-<div class="form-group">
-    {!! Form::label('q4', 'Q4. Describe your project? (500 Max)') !!}
-    <p>{!! $candidate->q4 !!}</p>
-</div>
+        <!-- Q4 Field -->
+        <div class="form-group">
+            {!! Form::label('q4', 'Q4. Describe your project? (500 Max)') !!}
+            <p>{!! $candidate->q4 !!}</p>
+        </div>
 
-<!-- Q5 Field -->
-<div class="form-group">
-    {!! Form::label('q5', 'Q5. How will you use the title to influence your community and africa at large once crowned Miss Career Africa? (500 Max)') !!}
-    <p>{!! $candidate->q5 !!}</p>
-</div>
+        <!-- Q5 Field -->
+        <div class="form-group">
+            {!! Form::label('q5', 'Q5. How will you use the title to influence your community and africa at large once
+            crowned Miss Career Africa? (500 Max)') !!}
+            <p>{!! $candidate->q5 !!}</p>
+        </div>
 
-<!-- Q6 Field -->
-<div class="form-group">
-    {!! Form::label('q6', 'Q6. Tell us why you deserve to be crowned Miss Career Africa? (500 Max)') !!}
-    <p>{!! $candidate->q6 !!}</p>
-</div>
+        <!-- Q6 Field -->
+        <div class="form-group">
+            {!! Form::label('q6', 'Q6. Tell us why you deserve to be crowned Miss Career Africa? (500 Max)') !!}
+            <p>{!! $candidate->q6 !!}</p>
+        </div>
 
-<!-- Q7 Field -->
-<div class="form-group">
-    {!! Form::label('q7', 'Q7. Briefly describe your recent career achievements you are proud of? (500 Max)') !!}
-    <p>{!! $candidate->q7 !!}</p>
-</div>
+        <!-- Q7 Field -->
+        <div class="form-group">
+            {!! Form::label('q7', 'Q7. Briefly describe your recent career achievements you are proud of? (500 Max)')
+            !!}
+            <p>{!! $candidate->q7 !!}</p>
+        </div>
 
-<!-- Session Id Field -->
-<div class="form-group">
-    {!! Form::label('session_id', 'Session Id:') !!}
-    <p>{!! $candidate->session_id !!}</p>
-</div>
 
-<!-- Votes Field -->
-<div class="form-group">
-    {!! Form::label('votes', 'Votes:') !!}
-    <p>{!! $candidate->votes !!}</p>
-</div>
 
-<!-- Is Selected Field -->
-<div class="form-group">
-    {!! Form::label('is_selected', 'Is Candidate Selected:') !!}
-    <p>{!! $candidate->is_selected?'<label class="badge badge-success">YES</label>':'<label class="badge badge-danger">NO</label>' !!}</p>
-</div>
+    </div>
+    <div class="col-md-8">
+        <!-- Session Id Field -->
+        <div class="form-group">
+            {!! Form::label('session_id', 'Session Id:') !!}
+            <p>{!! $candidate->session_id !!}</p>
+        </div>
 
-<!-- Created At Field -->
-<div class="form-group">
-    {!! Form::label('created_at', 'Created At:') !!}
-    <p>{!! $candidate->created_at !!}</p>
-</div>
+        <!-- Votes Field -->
+        <div class="form-group">
+            {!! Form::label('votes', 'Votes:') !!}
+            <p>{!! $candidate->votes !!}</p>
+        </div>
 
-<!-- Updated At Field -->
-<div class="form-group">
-    {!! Form::label('updated_at', 'Updated At:') !!}
-    <p>{!! $candidate->updated_at !!}</p>
-</div>
+        <!-- Is Selected Field -->
+        <div class="form-group">
+            {!! Form::label('is_selected', 'Is Candidate Selected:') !!}
+            <p>{!! $candidate->is_selected?'<label class="badge badge-success">YES</label>':'<label
+                    class="badge badge-danger">NO</label>' !!}</p>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <!-- Created At Field -->
+        <div class="form-group">
+            {!! Form::label('created_at', 'Created At:') !!}
+            <p>{!! $candidate->created_at !!}</p>
+        </div>
 
-</div>
+        <!-- Updated At Field -->
+        <div class="form-group">
+            {!! Form::label('updated_at', 'Updated At:') !!}
+            <p>{!! $candidate->updated_at !!}</p>
+        </div>
+    </div>
 </div>
