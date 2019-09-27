ee3b043548b9462d8a8fda077c7f1c2bbdc7eaa4
Richie457 <beastar457@gmail.com>
2019-09-27 11:11:59 +0200

    just in case

---
+++ resources/views/emails/notify.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+    Hello, {{$name}}
+    <br>
+    Thank your for applyin to become a misscareer.
+    <br>
+    <br>
+        We will notify you once you are selected.
+    <br>
+    
+    <br>
+
+    Best regards,<br>
+    The Miss Career Team.
+    </body>
+</html>
