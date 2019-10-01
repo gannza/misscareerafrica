5e714cb21e877b9730fe65c1f58f5cd088684098
Ganza respice <respinho2014@gmail.com>
2019-10-01 23:44:35 +0200

    ..

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,43 +87,48 @@ Facebook: 		https://www.facebook.com/yegobox
 
 <div id="fh5co-header">
 
-
 <header id="fh5co-header-section">
-					<div class="ml-3 mr-3">
-						<div class="nav-header">
-							<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
-							 <span id="fh5co-logo"><a href="/"><img src="images/logo.png" class="img-rounded"
-										style="width: 200px;height:80px;position: relative;bottom:25px"></a></span>
-							<nav id="fh5co-menu-wrap" role="navigation">
-								<ul class="sf-menu" id="fh5co-primary-menu">
-									<li><a href="/" style="color: #000;font-weight:bold;">HOME</a></li>
-									<li><a   style="color: #000;font-weight:bold;" href="candidate-application">APPLY NOW</a></li>
+                    <div class="container">
+                        <div class="nav-header">
+                            <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+                            <span id="fh5co-logo"><a href="/"><img src="images/logo.png" class="img-rounded"
+                                        style="width: 200px;height:80px;position: relative;bottom:25px"></a></span>
+                            <nav id="fh5co-menu-wrap" role="navigation">
+                                <ul class="sf-menu" id="fh5co-primary-menu">
+                                    <li><a href="/" style="color: #000;font-weight:bold">HOME</a></li>
+                                    <li><a class="apply" style="color: #000;font-weight:bold" href="candidate-application">APPLY NOW</a></li>
+                                    <li><a class="can-voting" style="color: #000;font-weight:bold" href="selected-candidates">CANDIDATES</a></li>
 
-									<li><a class="can-voting" style="color: #000;font-weight:bold;" href="selected-candidates">CANDIDATES</a></li>
 
-									<li><a style="color: #000;font-weight:bold;" href="eligibility">ELIGIBILITY</a></li>
 
-									<li><a   style="color: #000;font-weight:bold;" href="#">ORGANIZATION</a>
-										<ul class="fh5co-sub-menu">
-											<li><a href="blog">Our Blog</a></li>
-											<li><a href="scope">Our scope</a></li>
-											<li><a href="contact">Contact Us</a></li>
-										</ul>
-									</li>
-									<li><a  style="color: #000;font-weight:bold;" href="#">SUPPORT</a>
-										<ul class="fh5co-sub-menu">
-											<li><a href="volunteer">Become a volunteer</a></li>
-											<li><a href="sponsor">Become a sponsor</a></li>
-										</ul>
-									</li>
-                                    <li><a  href="scholarship" style="color: #000;font-weight:bold;">MCA SCHOLARSHIP</a></li>
-									<li><a  href="fund" style="color: #000;font-weight:bold;">MCA FUND</a></li>
-                                    <li><a  href="book-mca" style="color: #000;font-weight:bold;">Book MCA</a></li>
-								</ul>
-							</nav>
-						</div>
-					</div>
-				</header>
+                                    <li><a style="color: #000;font-weight:bold" href="#">ORGANIZATION</a>
+                                        <ul class="fh5co-sub-menu">
+                                            <li><a href="blog">Our Blog</a></li>
+                                            <li><a href="scope">Our scope</a></li>
+                                            <li><a href="contact">Contact Us</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a style="color: #000;font-weight:bold" href="#">SUPPORT</a>
+                                        <ul class="fh5co-sub-menu">
+                                            <li><a href="volunteer">Become a volunteer</a></li>
+                                            <li><a href="sponsor">Become a sponsor</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a style="color: #000;font-weight:bold" href="#">MORE....</a>
+                                        <ul class="fh5co-sub-menu">
+                                            <li><a href="scholarship">MCA SCHOLARSHIP</a></li>
+                                            <li><a href="fund">MCA FUND</a></li>
+                                            <li><a href="book-mca">Book MCA</a></li>
+                                            <li><a  href="eligibility">ELIGIBILITY</a></li>
+                                        </ul>
+                                    </li>
+
+
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </header>
 
 </div>
 
@@ -330,7 +335,9 @@ background-position: 50% 20%;margin-top: -15px;background-image: url(images/tab_
 						<ul class="link">
 								<li><a style="color: #fff; " href="volunteer">Become a volunteer</a></li>
 								<li><a style="color: #fff; " href="sponsor">Become a sponsor</a></li>
-								<li><a style="color: white;" href="scholarship">MCA Scholarship</a></li>
+								<li><a style="color: #000;font-weight:bold" href="scholarship">MCA Scholarship</a></li>
+                                <li><a style="color: #000;font-weight:bold" href="fund">MCA Fund</a></li>
+                                <li><a style="color: #000;font-weight:bold" href="book-mca">Book MCA</a></li>
 						</ul>
 					</div>
 
