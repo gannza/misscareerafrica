c022cb99d84be49dba61bdac52dc68c0a771485f
gannza <respinho2014@gmail.com>
2019-11-28 12:05:19 +0200

    update changes required

--- resources/views/candidate-info.blade.php
+++ resources/views/candidate-info.blade.php
@@ -136,54 +136,7 @@ Facebook: 		https://www.facebook.com/yegobox
                         </ul>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-12">
-                        <div class="row mt-5">
-                            <div class="col-md-3">
-                                <h3><b>ABOUT US</b></h3>
-                                <ul class="link">
-                                    <li><a style="color: white;" href="competition">The Competition</a></li>
-                                    <li><a style="color: white;" href="job">The Job</a></li>
-                                    <li><a style="color: white;" href="mission">Our Mission</a></li>
-
-                                </ul>
-                            </div>
-                            <div class="col-md-3">
-                                        <h3 class="apply"><b>MISS CAREER CANDIDATE</b></h3>
-                                        <ul class="link">
-                                            <li><a style="color: #fff;" class="apply" href="candidate-application">APPLY NOW</a></li>
-                                            <li><a style="color: #fff;" class="can-voting" href="selected-candidates">CANDIDATES</a></li>
-                                        </ul>
-                                </div>
-                                <div class="col-md-3">
-                                <h3><b>ORGANIZATION</b></h3>
-                                <ul class="link">
-                                    <li><a style="color: white;" href="blog">Our Blog</a></li>
-                                    <li><a style="color: white;" href="scope">Our Scope</a></li>
-                                    <li><a style="color: white;" href="contact">Contact Us</a></li>
-
-                                </ul>
-                            </div>
-
-                            <div class="col-md-3">
-                                <h3><b>SUPPORT</b></h3>
-                                <ul class="link">
-                                    <li><a style="color: white;" href="volunteer">Become a volunteer</a></li>
-                                    <li><a style="color: white;" href="sponsor">Become a sponsor</a></li>
-                                </ul>
-                            </div>
-
-                        </div>
-                        <div class="row mt-5">
-                            <div class="mx-auto col-6 text-center">
-                                <p> Copyright &copy; 2019 Miss Career Africa.</p>
-
-                            </div>
-                        </div>
-
-                    </div>
-
-                </div>
+               
             </div>
         </footer>
 
