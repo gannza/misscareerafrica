cad48eb488892c8da315ede32fe3089d32667934
gannza <respinho2014@gmail.com>
2019-11-28 12:02:15 +0200

    update changes required

--- resources/views/candidate-info.blade.php
+++ resources/views/candidate-info.blade.php
@@ -9,7 +9,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Preselected Candidate Details :: Miss Career</title>
+    <title>{{$candidate->fname}} {{$candidate->lname}} :: Miss Career</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Preselected Candidate Details" />
     <meta name="keywords" content="Miss Career, yegobox, flipper" />
