b65f751c95aedff175912fa4e4135998f7d002a0
gannza <respinho2014@gmail.com>
2019-11-28 23:06:07 +0200

    ..

--- resources/views/candidate-info.blade.php
+++ resources/views/candidate-info.blade.php
@@ -89,7 +89,7 @@ Facebook: 		https://www.facebook.com/yegobox
                     <div class="col-md-12">
                             <div class="card border-success mb-3" style="max-width: 100%">
                             <div class="card-header bg-transparent border-success"><b>{{$candidate->fname}} {{$candidate->lname}}</b></div>
-                                    <img class="img-fluid" src="{{$candidate->profile}}" style="width:100%;height:110vh;">
+                                    <img class="img-fluid" src="{{$candidate->profile}}" style="width:100%;">
                                     <div class="card-body text-success">
                                     <h5 class="card-title"><b>{{$candidate->city}} - {{$candidate->country}}</b></h5>
                                         <b class="card-text">
