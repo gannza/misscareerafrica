8a841ead83becde27246854f7f1245c9e63c2484
gannza <respinho2014@gmail.com>
2019-12-24 18:04:42 +0200

    change pictures

--- resources/views/past-candidates.blade.php
+++ resources/views/past-candidates.blade.php
@@ -37,14 +37,14 @@
                             <div class="card border-success mb-3" style="max-width: 100%">
                             <div class="card-header bg-transparent border-success"><b>{{$candidate->fname}} {{$candidate->lname}}</b></div>
                             <a href="candidate-page/{{$candidate->id}}">
-                                    <div class="img-fluid" style=" background-image: url('${element.profile}');
+                                    <div class="img-fluid" style=" background-image: url('{{$candidate->profile}}');
                                     background-repeat: no-repeat;width:100%;min-height:300px;
                                     background-size: cover; background-size: center center"></div>
                                     <div class="card-body text-success">
                                     <h5 class="card-title"><b>{{$candidate->city}} - {{$candidate->country}}</b></h5>
                                         <b class="card-text">
                                         <hr />
-                                       
+                                        <!-- {!! Str::limit($candidate->bio, 115, "<a href="candidate-page/{{$candidate->id}}">read more ....</a>") !!} -->
                                         </b>
                                     </div>
                             </a>
